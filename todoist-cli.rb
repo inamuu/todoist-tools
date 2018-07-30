44ed793867ac581a9ef2e88c3d91bded5f558808
inamuu <kzm0211@gmail.com>
2018-07-30 23:26:47 +0900

    :sparkles: trollopでCLIオプションを追加

--- todoist-cli.rb
+++ todoist-cli.rb
@@ -1,5 +1,18 @@
-def task(msg)
-  p msg
+require 'trollop'
+
+def tasks
+  puts "tasksだよ"
 end
 
-task(ARGV[0])
+opts = Trollop::options do
+  opt :tasks, 'Display all tasks'
+end
+
+#ETrollop::die "need at least one options" if ARGV.empty?
+
+p opts
+p opts[:tasks]
+
+if p opts[:tasks] == true
+  tasks
+end
