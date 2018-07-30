a8869485c7b5e41d24e04c14fe263703564a3795
inamuu <kzm0211@gmail.com>
2018-07-31 00:00:20 +0900

    :+1: tasksメソッドが実行されるように分岐を追加

--- todoist-cli.rb
+++ todoist-cli.rb
@@ -8,11 +8,6 @@ opts = Trollop::options do
   opt :tasks, 'Display all tasks'
 end
 
-Trollop::die "need at least one options" if ARGV.empty?
+#Trollop::die "need at least one options" if ARGV.empty?
 
-p opts
-p opts[:tasks]
-
-if opts.has_key?[:tasks]
-  tasks
-end
+tasks if opts[:tasks] == true
