0bdf52c839e8738f660452f92e8c1b6e92618969
inamuu <kzm0211@gmail.com>
2019-02-03 19:08:03 +0900

    :gems: itemsを取得できるように

--- todoist-cli.rb
+++ todoist-cli.rb
@@ -26,9 +26,31 @@ def projects
   hash[:projects].map { | hash | puts hash[:name] }
 end
 
+def items
+  uri = URI.parse(ENV["apiurl"])
+  request = Net::HTTP::Post.new(uri)
+  request.set_form_data(
+    "resource_types" => "[\"items\"]",
+    "token" => ENV["apitoken"]
+  )
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do | http |
+    http.request(request)
+  end
+
+  hash = JSON.parse(response.body,  symbolize_names: true)
+  hash[:items].map { | hash | puts hash[:content] }
+end
+
 opts = Trollop::options do
   opt :projects, 'プロジェクト一覧を表示する'
+  opt :items, 'タスク一覧を表示する'
 end
 
 Trollop::die "オプションを指定してください" unless opts.values.any?
 projects if opts[:projects] == true
+items if opts[:items] == true
