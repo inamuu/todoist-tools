d87180a8ff26d62360fba61642c39864fc6ecc9c
inamuu <kzm0211@gmail.com>
2019-02-03 17:44:13 +0900

    :gems: プロジェクト一覧を取得する

--- todoist-cli.rb
+++ todoist-cli.rb
@@ -1,7 +1,26 @@
 require 'trollop'
+require 'dotenv'
+require 'net/http'
+require 'uri'
+
+Dotenv.load
 
 def tasks
-  puts "tasksだよ"
+  uri = URI.parse(ENV["apiurl"])
+  request = Net::HTTP::Post.new(uri)
+  request.set_form_data(
+    "resource_types" => "[\"projects\"]",
+    "token" => ENV["apitoken"]
+  )
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do | http |
+    http.request(request)
+  end
+  puts response.body
 end
 
 opts = Trollop::options do
