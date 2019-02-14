92e966c980eb5978b643896dcf0e2adf191c6735
inamuu <8310973+inamuu@users.noreply.github.com>
2019-02-15 01:47:49 +0900

    :+1: サブコマンドをやめて位置引数だけ使用するように変更

--- python/main.py
+++ python/main.py
@@ -10,12 +10,13 @@ load_dotenv(dotenv_path)
 api = TodoistAPI(os.environ.get("APITOKEN"))
 api.sync()
 
-def projects():
+def projects(args):
+  print(args)
   list = api.state['projects']
   for name in list:
     print(name['name'])
 
-def tasks():
+def tasks(args):
   items = api.state['items']
   for name in items:
     if name['project_id'] == 2190973782:
@@ -24,22 +25,17 @@ def tasks():
 def main():
   parser = argparse.ArgumentParser(
     prog='CLIツール',
-    usage='python3 main2.py サブコマンド',
+    usage='python3 main2.py オプション',
     description='CLIツールのテストです',
     add_help=True,
   )
   
   parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-  subparsers = parser.add_subparsers(dest='[エラー内容] サブコマンド必須です。 -h, --help で確認してください。', title='subcomands')
-  subparsers.required = True
-  
-  parser_projects = subparsers.add_parser('projects', help='projectsだよ')
-  parser_projects.set_defaults(fn=projects)
-  
-  parser_tasks = subparsers.add_parser('tasks', help='tasksだよ')
-  parser_tasks.set_defaults(fn=tasks)
-
+  parser.add_argument('-p', '--projects')
+  parser.add_argument('-t', '--tasks')
   args = parser.parse_args()
-  args.fn()
+
+  if args.projects: projects(args)
+  if args.tasks: tasks(args)
 
 if __name__ == "__main__": main()
