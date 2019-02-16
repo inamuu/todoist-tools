da5e7d408fa4bd2c8a495ccd260c0f770b6ab12d
inamuu <8310973+inamuu@users.noreply.github.com>
2019-02-16 22:11:15 +0900

    :elephant: ヘルプメッセージを変更

--- python/main.py
+++ python/main.py
@@ -10,12 +10,12 @@ load_dotenv(dotenv_path)
 api = TodoistAPI(os.environ.get("APITOKEN"))
 api.sync()
 
-def projects(args):
+def projects():
   list = api.state['projects']
   for name in list:
     print(name['name'])
 
-def tasks(args):
+def tasks():
   items = api.state['items']
   for name in items:
     if name['project_id'] == 2190973782:
@@ -29,12 +29,12 @@ def main():
     add_help=True,
   )
   
-  parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-  parser.add_argument('-p', '--projects', action='store_true')
-  parser.add_argument('-t', '--tasks', choices=['apple', 'banana', 'orange'])
+  parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+  parser.add_argument('-p', '--projects', action='store_true', help='プロジェクト一覧を表示します')
+  parser.add_argument('-t', '--tasks', help='タスク一覧を表示します')
   args = parser.parse_args()
 
-  if args.projects: projects(args)
-  if args.tasks: tasks(args)
+  if args.projects: projects()
+  if args.tasks: tasks()
 
 if __name__ == "__main__": main()
